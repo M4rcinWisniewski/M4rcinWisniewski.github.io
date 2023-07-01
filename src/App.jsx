79fbcaa8d32e05f73f904910e06f78432bfe1bf3
Marcin <marcinwisniewski707@gmail.com>
2023-07-02 00:35:58 +0200

    added chessboard

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import PlayRandomMoveEngine from './components/chessboard';
+import './style/board.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PlayRandomMoveEngine  />
+    </div>
+  );
+}
+
+export default App;
